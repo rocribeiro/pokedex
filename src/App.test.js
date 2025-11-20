0036b1747a665a75c124cb03b84e0959be55495a
rodrigo <rodrigo.ribeiro03@hotmail.com>
2025-11-19 22:22:23 -0300

    ajustando testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,32 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+const mockPokemon = {
+  name: 'bulbasaur',
+  height: 7,
+  weight: 69,
+  sprites: {
+    front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  },
+};
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockPokemon),
+    })
+  );
+});
+
+test('renders initial message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Pokedex/i);
+  const linkElement = screen.getByText(/Selecione um Pokémon para ver suas informações/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('gets a random pokemon', async () => {
+test('gets a new pokemon', async () => {
   render(<App />);
-  const button = screen.getByText(/Get Random Pokémon/i);
+  const button = screen.getByText(/Novo Pokémon/i);
   fireEvent.click(button);
   const pokemonName = await screen.findByText(/bulbasaur/i);
   expect(pokemonName).toBeInTheDocument();
@@ -17,11 +34,11 @@ test('gets a random pokemon', async () => {
 
 test('gets pokedex info', async () => {
   render(<App />);
-  const randomButton = screen.getByText(/Get Random Pokémon/i);
+  const randomButton = screen.getByText(/Novo Pokémon/i);
   fireEvent.click(randomButton);
   await screen.findByText(/bulbasaur/i);
-  const pokedexButton = screen.getByText(/Get Pokedex Info/i);
+  const pokedexButton = screen.getByText(/Scanear Pokémon/i);
   fireEvent.click(pokedexButton);
-  const height = await screen.findByText(/Height/i);
+  const height = await screen.findByText(/Altura/i);
   expect(height).toBeInTheDocument();
 });
